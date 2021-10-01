cbc37db62c276f4ac7bc7c3e4d54d05fc62700ce
bolub <abiol5202@gmail.com>
2021-10-01 10:09:01 +0100

    aDD LINKS

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -23,22 +23,36 @@ const Navbar = () => {
 
   return (
     <nav className='px-5 md:px-16 mt-5 py-6 sticky top-0 flex bg-white'>
-      <span id='logo' className='font-extrabold text-2xl my-auto'>
+      <span id='logo' className='font-bold text-2xl my-auto mr-5'>
         <span className='text-purple-500'>Dash</span>
 
-        <span className='text-yellow-500'>Create</span>
+        <span style={{ color: 'rgba(245, 158, 11' }}>Create</span>
       </span>
 
-      {scrollY > 300 && (
-        <button
-          className='transition duration-100 border-purple-500 border border-transparent text-purple-500 py-3 px-4
-            
-            rounded-sm font-semibold ml-auto '
-          onClick={() => openLink('https://Bit.ly/bidc1')}
+      <div className='ml-auto my-auto'>
+        <a
+          href='#instructors'
+          className='mr-6 cursor-pointer text-gray-600 font-medium hover:text-purple-500'
         >
-          Register now
-        </button>
-      )}
+          Instructors
+        </a>
+        <a
+          href='#whatYouLearn'
+          className=' cursor-pointer text-gray-600 font-medium hover:text-purple-500'
+        >
+          What You'll Learn
+        </a>
+        {scrollY > 300 && (
+          <button
+            className='ml-6 transition duration-100 border-purple-500 border border-transparent text-purple-500 py-3 px-4
+            
+            rounded-sm font-semibold '
+            onClick={() => openLink('https://Bit.ly/bidc1')}
+          >
+            Register now
+          </button>
+        )}
+      </div>
     </nav>
   );
 };
